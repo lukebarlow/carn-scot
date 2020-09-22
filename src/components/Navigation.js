f75470ae978da435e597267ab78a24d0d6901f55
Luke Barlow <luke.barlow@gmail.com>
2020-09-22 17:00:35 +0100

    fixing navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,6 @@
 // links across the top on desktop, hamburger menu on mobile
 import React from 'react'
 
-// const sections = [
-//   ['gallery', 'Gallery'],
-//   ['info', 'Info'],
-//   ['3d', '3D'],
-//   ['cook', 'Cook'],
-//   ['build', 'Build']
-// ]
-
 const linkStyle = {
   color: 'white',
   paddingLeft: 30
@@ -18,7 +10,7 @@ export default ({ pages }) => {
   return (
     <div className='navigation-container'>
       {pages.map(({ slug, title }) => (
-        <a key={slug} href={slug} style={linkStyle}>
+        <a key={slug} href={'./' + slug} style={linkStyle}>
           {title}
         </a>
       ))}
